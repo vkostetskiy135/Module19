9e5ff1806caa6277bbb0858779c4ed0952330a5a
vkost <vkostetskiy135@gmail.com>
2024-09-21 12:44:33 +0300

    task1

---
+++ task1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.forms import DecimalField
+
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
